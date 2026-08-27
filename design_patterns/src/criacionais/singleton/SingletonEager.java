141d6de6f1a2c677d4f6ed994d7e8380d03cff2a
Engenheiro Muniz <engenheiro.muniz@outlook.com>
2026-08-27 09:18:39 -0300

    docs: Foram inseridad definições na classe SingletonEager

--- design_patterns/src/criacionais/singleton/SingletonEager.java
+++ design_patterns/src/criacionais/singleton/SingletonEager.java
@@ -2,6 +2,21 @@ package criacionais.singleton;
 
 public class SingletonEager {
 	
+	/*
+	 * Modelo 1 - Eager Inicialization (podemos entender como uma inicialização antecipada "ansiosa")
+	 *   - Nesse caso a instância será criada assim assim que a classe for carregada pela JVM, independente de chamada getInstance().
+	 *   
+	 *   ✅ Vantagem: garante a segurança das treads evitando a concorrência entre elas.
+	 *   
+	 *   ❌ Desvantagem: se o objeto não for usado, estará desperdiçando recursos
+	 *   
+	 *   - - - Definições
+	 *   - CLASSLOADER: mecanismo da JVM de carregar os arquivos .class
+	 *   - 
+	 *   
+	 *   
+	 * */
+	
 	private static final SingletonEager instancia = new SingletonEager();
 	private int contador = 0;
 	
